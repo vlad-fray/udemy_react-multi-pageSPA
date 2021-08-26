2c09a2d51da9582cfaa953caf59e12853d5aca9b
VFray <rekrut313@gmail.com>
2021-08-26 16:55:42 +0300

    i`m tired

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,4 +1,5 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import { Prompt } from 'react-router-dom';
 
 import Card from '../UI/Card';
 import LoadingSpinner from '../UI/LoadingSpinner';
@@ -7,8 +8,9 @@ import classes from './QuoteForm.module.css';
 const QuoteForm = (props) => {
   const authorInputRef = useRef();
   const textInputRef = useRef();
+  const [isEntering, setIsEntering] = useState(false);
 
-  function submitFormHandler(event) {
+  const submitFormHandler = (event) => {
     event.preventDefault();
 
     const enteredAuthor = authorInputRef.current.value;
@@ -17,30 +19,51 @@ const QuoteForm = (props) => {
     // optional: Could validate here
 
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
-  }
+  };
+
+  const finishEnteringHandler = () => {
+    setIsEntering(false);
+  };
+
+  const formFocusHandler = () => {
+    setIsEntering(true);
+  };
 
   return (
-    <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
-        {props.isLoading && (
-          <div className={classes.loading}>
-            <LoadingSpinner />
-          </div>
-        )}
+    <>
+      <Prompt when={isEntering} message={(loc) => 'Are you sure?'} />
+      <Card>
+        <form
+          onFocus={formFocusHandler}
+          className={classes.form}
+          onSubmit={submitFormHandler}
+        >
+          {props.isLoading && (
+            <div className={classes.loading}>
+              <LoadingSpinner />
+            </div>
+          )}
 
-        <div className={classes.control}>
-          <label htmlFor='author'>Author</label>
-          <input type='text' id='author' ref={authorInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='text'>Text</label>
-          <textarea id='text' rows='5' ref={textInputRef}></textarea>
-        </div>
-        <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
-        </div>
-      </form>
-    </Card>
+          <div className={classes.control}>
+            <label htmlFor='author'>Author</label>
+            <input type='text' id='author' ref={authorInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor='text'>Text</label>
+            <textarea
+              id='text'
+              rows='5'
+              ref={textInputRef}
+            ></textarea>
+          </div>
+          <div className={classes.actions}>
+            <button onClick={finishEnteringHandler} className='btn'>
+              Add Quote
+            </button>
+          </div>
+        </form>
+      </Card>
+    </>
   );
 };
 
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,4 +1,9 @@
-import { Route, useParams } from 'react-router-dom';
+import {
+  Link,
+  Route,
+  useParams,
+  useRouteMatch,
+} from 'react-router-dom';
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from './../components/quotes/HighlightedQuote';
 
@@ -8,6 +13,7 @@ const DUMMY_QUOTES = [
 ];
 
 const QuotesDetail = () => {
+  const match = useRouteMatch();
   const params = useParams();
   const quote = DUMMY_QUOTES.find(
     (quote) => quote.id === params.quoteId
@@ -16,7 +22,16 @@ const QuotesDetail = () => {
   const quoteMarkup = (
     <>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+
+      <Route path={match.path} exact>
+        <div className='centered'>
+          <Link className='btn--flat' to={`${match.url}/comments`}>
+            Load comments
+          </Link>
+        </div>
+      </Route>
+
+      <Route path={`${match.path}/comments`}>
         <Comments comments={[]} />
       </Route>
     </>
