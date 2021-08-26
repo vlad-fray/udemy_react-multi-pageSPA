5630211ead57e603fd339a04516167cc44feabe7
VFray <rekrut313@gmail.com>
2021-08-26 10:26:29 +0300

    rebuild

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-import { Redirect, Route } from 'react-router-dom';
-import MainHeader from './components/MainHeader';
-import ProductDetail from './pages/ProductDetail';
-import Products from './pages/Products';
-import Welcome from './pages/Welcome';
-
 function App() {
   return (
     <div>
-      <MainHeader />
-      <main>
-        <Route path='/' exact>
-          <Redirect to='/welcome' />
-        </Route>
-        <Route path='/welcome'>
-          <Welcome />
-        </Route>
-        <Route path='/products' exact>
-          <Products />
-        </Route>
-        <Route path='/products/:productId'>
-          <ProductDetail />
-        </Route>
-      </main>
+
     </div>
   );
 }
